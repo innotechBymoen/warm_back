8dfdccad98f0225586416dae1c36e7c0670afb1a
abymoen <a.bymoen@hotmail.com>
2022-10-18 17:47:17 -0600

    Fixed modes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import request, Flask
+from flask import request, Flask, make_response
 import json
 from dbhelpers import run_statement
 from apihelpers import check_endpoint_info
+import dbcreds
 
 app = Flask(__name__)
 
@@ -10,9 +11,17 @@ def get_restaurant():
     results = run_statement('CALL get_gifs()')
     if(type(results) == list):
         res_json = json.dumps(results, default=str)
-        return res_json
+        return make_response(res_json, 200)
     else:
-        return "Sorry, there was an error getting the gifs"
+        return make_response("Sorry, there was an error getting the gifs", 500)
 
 # FIX ME WITH PROPER RUNNING!
-app.run(debug=True)
+if(dbcreds.production_mode == True):
+    print("Running in Production Mode")
+    import bjoern # type: ignore
+    bjoern.run(app, "0.0.0.0", 5063)
+else:
+    from flask_cors import CORS
+    CORS(app)
+    print("Running in Testing Mode!")
+    app.run(debug=True)
